b9c1584ac2154c8221e5028c6d54dfa67a5e2873
Greg Smith <gregsmith446@gmail.com>
2019-01-22 11:36:35 -0500

    used module to print

--- NextLevelTicTacToe/Program.cs
+++ NextLevelTicTacToe/Program.cs
@@ -10,6 +10,47 @@ namespace NextLevelTicTacToe
     {
         static void Main(string[] args)
         {
+            // create an instance of the MakeBoard class
+            var callClass = new MakeBoard();
+
+            // use the callClass variable to access on the ticTac function
+            callClass.ticTac();
+        }
+    }
+
+    class MakeBoard
+    {
+        public string ticTac()
+        {
+            string line1 = "x|x|x";
+            string line2 = "-+-+-";
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (i%2 == 0)
+                {
+                    Console.Write(line1 + "\n");
+                }
+                else
+                {
+                    Console.Write(line2 + "\n");
+                }
+            }
+
+            return "yes, you have made a tic tac toe board";
+
         }
     }
 }
+
+/* 
+Create the following function/method:
+drawBoard()
+ | |
+-+-+-
+ | |
+-+-+-
+ | |
+*/
+
+  
