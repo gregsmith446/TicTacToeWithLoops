3ddde96f75120d94078aa425d8d7a67df0606565
Greg Smith <gregsmith446@gmail.com>
2019-01-22 22:46:23 -0500

    working through next step

--- NextLevelTicTacToe/Program.cs
+++ NextLevelTicTacToe/Program.cs
@@ -14,43 +14,50 @@ namespace NextLevelTicTacToe
             var callClass = new MakeBoard();
 
             // use the callClass variable to access on the ticTac function
-            callClass.ticTac();
+            // callClass.ticTac();
+
+            string[] input = { "x", "o", "x", "o", "x", "x", " ", "x", "x" };
+
+            // use callClass var to access the importBoard function
+            callClass.importBoard(input);
         }
     }
 
     class MakeBoard
     {
-        public string ticTac()
+        public string importBoard(string[] input)
         {
-            string line1 = "x|x|x";
+            char[] line1 = { '|', '|', ' '};
             string line2 = "-+-+-";
 
             for (int i = 0; i < 5; i++)
             {
-                if (i%2 == 0)
+                if (i % 2 == 0)
                 {
-                    Console.Write(line1 + "\n");
+                    foreach (char item in line1)
+                    {
+                        for (int k = 0; k < input.Length; k++)
+                        {
+                            Console.Write(input[k] + item);
+                        }
+                    }
+                    Console.WriteLine();
                 }
                 else
                 {
                     Console.Write(line2 + "\n");
                 }
             }
-
-            return "yes, you have made a tic tac toe board";
-
+            return "data";
         }
     }
 }
 
-/* 
-Create the following function/method:
-drawBoard()
- | |
--+-+-
- | |
--+-+-
- | |
-*/
+        // accepts 9 comma separated characters/strings
+        // data will be either '' 'x' 'o'
 
+        // the indexes of the array
+        // 0 1 2
+        // 3 4 5
+        // 6 7 8
   
